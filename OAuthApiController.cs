4cc169424e6d928d5caebb596a382e71f37d27a4
Vahid Radkoosha <vahidradkoosha@gmail.com>
2017-11-30 12:56:13 -0800

    Add files via upload

---
+++ OAuthApiController.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json.Linq;
+using Sabio.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Sabio.Web.Controllers.Api
+{
+    [RoutePrefix("api/oauth")]
+    public class OAuthApiController : ApiController
+    {
+        readonly IOAuthService oAuthService;
+        readonly IUserService userService;
+
+        public OAuthApiController(IOAuthService oAuthService, IUserService userService)
+        {
+            this.oAuthService = oAuthService;
+            this.userService = userService;
+        }
+
+        [AllowAnonymous]
+        [Route("verifytheuser"), HttpGet]
+        public HttpResponseMessage GetByEmailAndOAuthId(string userEmail = null, string id_token = null, string oAuthId = null, ProviderEnum? provider = null)
+        {
+            bool OAuthAvailability = false;
+
+            if (provider == ProviderEnum.Google)
+            {
+                OAuthAvailability = oAuthService.CheckGoogleTokenId(id_token, oAuthId);
+            }
+            else if (provider == ProviderEnum.Facebook)
+            {
+                OAuthAvailability = oAuthService.CheckFacebookTokenId(id_token, oAuthId);
+            }
+
+            if (OAuthAvailability)
+            {
+                bool isRegistered = userService.GetByEmailAndOAuhtId(oAuthId);
+
+                return Request.CreateResponse(HttpStatusCode.OK, isRegistered ? "REGISTERED" : "NOT_REGISTERED");
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "BAD_REQUEST");
+            }
+        }
+    }
+}
---
+++ OAuthService.cs
@@ -0,0 +1,120 @@
+﻿using Newtonsoft.Json.Linq;
+using Sabio.Data;
+using Sabio.Data.Providers;
+using Sabio.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Facebook;
+using System.Configuration;
+using System.Web;
+
+namespace Sabio.Services
+{
+    public class OAuthService : IOAuthService
+    {
+        readonly IDataProvider dataProvider;
+        readonly OAuthKeys oAuthKeys;
+
+        public OAuthService(IDataProvider dataProvider, OAuthKeys oAuthKeys)
+        {
+            this.dataProvider = dataProvider;
+            this.oAuthKeys = oAuthKeys;
+        }
+
+        public bool CheckGoogleTokenId(string id_token, string oAuthId)
+        {
+            string endPoint = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + HttpUtility.UrlEncode(id_token);
+            string googleContentId = oAuthKeys.GoogleContentId;
+
+            try
+            {
+                WebClient client = new WebClient();
+                string reply = client.DownloadString(endPoint);
+                var data = JObject.Parse(reply);
+                string userId = data["sub"].ToString();
+                string userEmail = data["email"].ToString();
+                string gContentId = data["azp"].ToString();
+
+                if (userId == oAuthId && gContentId == googleContentId)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+
+        public bool CheckFacebookTokenId(string tokenToInspect, string oAuthId)
+        {
+            string app_Id = oAuthKeys.FacebookAppId;
+            string app_secret = oAuthKeys.FacebookAppSecret;
+
+            var fb = new FacebookClient();
+            dynamic result = fb.Get("oauth/access_token", new
+            {
+                client_id = app_Id,
+                client_secret = app_secret,
+                grant_type = "client_credentials"
+            });
+
+            fb.AccessToken = result.access_token;
+
+            string endPoint = string.Format("https://graph.facebook.com/debug_token?input_token={0}&access_token={1}", HttpUtility.UrlEncode(tokenToInspect), HttpUtility.UrlEncode(fb.AccessToken));
+
+            try
+            {
+                WebClient client = new WebClient();
+                var reply = client.DownloadString(endPoint);
+                var data = JObject.Parse(reply);
+                string appId = data["data"]["app_id"].ToString();
+                string userId = data["data"]["user_id"].ToString();
+
+                if (appId == app_Id && userId == oAuthId)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+
+        public bool GetByEmailAndOAuhtId(string userEmail, string OAuthId)
+        {
+            OAuth_VerifyAvailabilityResponse response = null;
+            dataProvider.ExecuteCmd("dbo.Users_OAuthValidation"
+                , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+                {
+                    paramCollection.AddWithValue("@userEmail", userEmail);
+                    paramCollection.AddWithValue("@oAuthId", OAuthId);
+                }
+                , singleRecordMapper: delegate (IDataReader reader, short set)
+                {
+                    response = new OAuth_VerifyAvailabilityResponse();
+
+                    response.IsRegistered = reader.GetSafeInt32(0) == 1 ? true : false;
+                });
+
+            return response.IsRegistered;
+        }
+    }
+}
